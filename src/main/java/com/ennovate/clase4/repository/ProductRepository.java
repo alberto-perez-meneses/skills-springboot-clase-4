1a60439e989ea6906c1eb5dfa3ad8f5fcfa1698d
Alberto Pérez Meneses <56211766+alberto-perez-meneses@users.noreply.github.com>
2025-11-18 07:31:49 -0600

    Revert "mapeo de tabla de productos"

--- src/main/java/com/ennovate/clase4/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ennovate.clase4.repository;
-
-import com.ennovate.clase4.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
